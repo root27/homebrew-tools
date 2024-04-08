2914d3a602918a7431c65b8baae77485b603e7c6
root27 <oguzhaneee@gmail.com>
2024-04-08 18:56:02 +0300

    added filecrypt

---
+++ filecrypt.rb
@@ -0,0 +1,16 @@
+class Filecrypt < Formula
+    desc "File Encryption and Decryption"
+    homepage "https://github.com/root27/filecrypt"
+    version "0.1.4"
+
+    on_macos do 
+        if Hardware::CPU.intel?
+            url "https://github.com/root27/filecrypt/releases/download/v0.1.4/filecrypt-x86_64-apple-darwin.tar.gz"
+            sha256 "39676102c0deedf866e3f4a56b6a55d74a2ed37935da0cb1c5e450cd2658678b"
+
+            def install 
+                bin.install "filecrypt"
+            end
+        end
+
+    end
