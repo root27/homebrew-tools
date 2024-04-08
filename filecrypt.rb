032371271b477a2977cf7868876b737beb7eb230
Oguzhan Dogan <oguzhaneee@gmail.com>
2024-04-08 12:52:12 +0300

    Update filecrypt.rb

--- filecrypt.rb
+++ filecrypt.rb
@@ -2,7 +2,7 @@ class Filecrypt < Formula
   desc "File Encryption and Decryption"
   homepage "https://github.com/root27/filecrypt"
   url "https://github.com/root27/filecrypt/releases/download/v0.1.3/filecrypt-x86_64-apple-darwin.tar.gz"
-  sha256 "4208893d880e6d3613f368a8d75e2d52fe32b30e5a627b19406852d43d0bec7f"
+  sha256 "418016c9ec9f7c31639366bc2a3e52e4ad657760"
   version "0.1.3"
 
   def install
